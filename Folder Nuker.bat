71c3a9c6854b25658070b448c695c6946e8685d5
Andrew Park <40168823+aparkgh@users.noreply.github.com>
2024-10-02 14:05:04 +1000

    Add Folder Nuker

---
+++ Folder Nuker.bat
@@ -0,0 +1,22 @@
+REM                      .___       ___.                                      __    
+REM     _____ _____    __| _/____   \_ |__ ___.__. _____  ___________ _______|  | __
+REM    /     \\__  \  / __ _/ __ \   | __ <   |  | \__  \ \____ \__  \\_  __ |  |/ /
+REM   |  Y Y  \/ __ \/ /_/ \  ___/   | \_\ \___  |  / __ \|  |_> / __ \|  | \|    < 
+REM   |__|_|  (____  \____ |\___  >  |___  / ____| (____  |   __(____  |__|  |__|_ \
+REM         \/     \/     \/    \/       \/\/           \/|__|       \/           \/
+
+REM thank you for checking out my github!
+REM this is one of the first batch projects i ever made
+REM i wanted it to always be part of my github
+REM so here it is enjoy
+
+
+@echo off
+title Folder Nuker by aparkgh
+
+echo Press any key to start nuking!
+pause>nul
+
+:1
+md %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto 1
