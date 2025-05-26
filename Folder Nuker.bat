4a6a2b887851f1372b988180809420785fa38fd4
Andrew Park <40168823+aparkgh@users.noreply.github.com>
2025-05-27 07:52:46 +1000

    edit comments

--- Folder Nuker.bat
+++ Folder Nuker.bat
@@ -1,15 +1,7 @@
-REM                      .___       ___.                                      __    
-REM     _____ _____    __| _/____   \_ |__ ___.__. _____  ___________ _______|  | __
-REM    /     \\__  \  / __ _/ __ \   | __ <   |  | \__  \ \____ \__  \\_  __ |  |/ /
-REM   |  Y Y  \/ __ \/ /_/ \  ___/   | \_\ \___  |  / __ \|  |_> / __ \|  | \|    < 
-REM   |__|_|  (____  \____ |\___  >  |___  / ____| (____  |   __(____  |__|  |__|_ \
-REM         \/     \/     \/    \/       \/\/           \/|__|       \/           \/
-
 REM thank you for checking out my github!
 REM this is one of the first batch projects i ever made
 REM i wanted it to always be part of my github
-REM so here it is enjoy
-
+REM so here it is, enjoy :)
 
 @echo off
 title Folder Nuker by aparkgh
